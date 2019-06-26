41ddd13c654b9b3ff31e891850de460d166defd1
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2019-06-26 17:24:56 +0100

    Delete MatrixOperator.jl

--- src/Operators/banded/MatrixOperator.jl
+++
@@ -1,8 +0,0 @@
-function Conversion(A::Array{Space,1},B::Array{Space,1},plan::Array{Int64,1})
-    @assert size(A)==size(B) && size(A)==size(plan) # lazy assertion
-    ret=[ZeroOperator(AA,BB) for BB in B, AA in A]
-    for k in 1:length(A)
-        ret[plan[k],k]=Conversion(A[k],B[k])
-    end
-    ret
-end
