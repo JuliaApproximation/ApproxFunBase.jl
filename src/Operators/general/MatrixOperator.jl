47d302da0fe10bffd017bd5f02a32a621dc704c7
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2019-06-26 21:56:22 +0100

    Update MatrixOperator.jl

--- src/Operators/general/MatrixOperator.jl
+++ src/Operators/general/MatrixOperator.jl
@@ -1,8 +1,8 @@
 function Conversion(A::Array{<:Any,1},B::Array{<:Any,1},plan::Array{Int64,1})
     @assert size(A)==size(B) && size(A)==size(plan) # lazy assertion
-    ret=Any[ZeroOperator{promote_type(eltype(AA),eltype(BB))}(AA,BB) for BB in B, AA in A]
+    ret=Any[ZeroOperator(promote_type(prectype(AA),prectype(BB)),AA,BB) for BB in B, AA in A]
     for k in 1:length(A)
-        ret[plan[k],k]=Conversion(A[k],B[k])
+        ret[plan[k],k]=Conversion(A[k],B[plan[k]])
     end
     ret
 end
