eb05ce11de66e7e53cf903389dd75e7a2f24a6d8
Sheehan Olver <solver@mac.com>
2019-09-01 09:43:37 +0100

    Fix diagonal container type in 1.3

--- src/Operators/systems.jl
+++ src/Operators/systems.jl
@@ -26,31 +26,40 @@ end
 
 
 ## Construction
-function diagm_container(kv::Pair{<:Integer,<:AbstractVector{O}}...) where O<:Operator
-    T = mapreduce(x -> mapreduce(eltype,promote_type,x.second),
-                  promote_type, kv)
-    n = mapreduce(x -> length(x.second) + abs(x.first), max, kv)
-    zeros(Operator{T}, n, n)
+if VERSION < v"1.3-"
+  function diagm_container(kv::Pair{<:Integer,<:AbstractVector{O}}...) where O<:Operator
+      T = mapreduce(x -> mapreduce(eltype,promote_type,x.second),
+                    promote_type, kv)
+      n = mapreduce(x -> length(x.second) + abs(x.first), max, kv)
+      zeros(Operator{T}, n, n)
+  end
+else
+    function diagm_container(size, kv::Pair{<:Integer,<:AbstractVector{O}}...) where O<:Operator
+        T = mapreduce(x -> mapreduce(eltype,promote_type,x.second),
+                      promote_type, kv)
+        n = mapreduce(x -> length(x.second) + abs(x.first), max, kv)
+        zeros(Operator{T}, n, n)
+    end
 end
-
+  
 ##TODO: unify with other blockdiag
 function blockdiag(d1::AbstractVector{T},d2::AbstractVector{T}) where T<:Operator
-  if isempty(d1)&&isempty(d2)
-    error("Empty blockdiag")
-  end
-  if isempty(d1)
-    TT=mapreduce(eltype,promote_type,d2)
-  elseif isempty(d2)
-    TT=mapreduce(eltype,promote_type,d1)
-  else
-     TT=promote_type(mapreduce(eltype,promote_type,d1),
-                    mapreduce(eltype,promote_type,d2))
-  end
+    if isempty(d1)&&isempty(d2)
+        error("Empty blockdiag")
+    end
+    if isempty(d1)
+        TT=mapreduce(eltype,promote_type,d2)
+    elseif isempty(d2)
+        TT=mapreduce(eltype,promote_type,d1)
+    else
+        TT=promote_type(mapreduce(eltype,promote_type,d1),
+                        mapreduce(eltype,promote_type,d2))
+    end
 
-  D=zeros(Operator{TT},length(d1)+length(d2),2)
-  D[1:length(d1),1]=d1
-  D[length(d1)+1:end,2]=d2
-  D
+      D=zeros(Operator{TT},length(d1)+length(d2),2)
+      D[1:length(d1),1]=d1
+      D[length(d1)+1:end,2]=d2
+      D
 end
 
 blockdiag(a::Operator,b::Operator) = blockdiag(Operator{promote_type(eltype(a),eltype(b))}[a],
