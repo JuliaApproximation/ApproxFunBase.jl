2d20fc75f2dce4c4483573cce0b215c537da3907
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-08-19 16:11:57 +0400

    convert to vector in chop

--- test/SpacesTest.jl
+++ test/SpacesTest.jl
@@ -1,6 +1,7 @@
 using ApproxFunBase, Test
 import ApproxFunBase: PointSpace, HeavisideSpace, PiecewiseSegment, dimension, Vec, checkpoints
 using ApproxFunOrthogonalPolynomials
+using StaticArrays
 
 @testset "Spaces" begin
     @testset "PointSpace" begin
@@ -110,6 +111,11 @@ using ApproxFunOrthogonalPolynomials
             @test eltype(coefficients(g)) == Float32
             g = @inferred (x -> x^2)(f)
             @test eltype(coefficients(g)) == Float32
+        end
+
+        @testset "static coeffs" begin
+            f = Fun(PointSpace(1:3), SA[1,2,3])
+            @test coefficients(f^2) == coefficients(f).^2
         end
     end
 
