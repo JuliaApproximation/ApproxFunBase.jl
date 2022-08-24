d255eceb74dddf041a70e0d08613fd3e66c8631b
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-08-24 11:52:46 +0400

    add OneHotVector

---
+++ src/onehotvector.jl
@@ -0,0 +1,12 @@
+export OneHotVector
+
+struct OneHotVector{T} <: AbstractVector{T}
+	n :: Int
+	len :: Int
+end
+OneHotVector(n, len = n) = OneHotVector{Float64}(n, len)
+Base.size(v::OneHotVector) = (v.len,)
+Base.length(v::OneHotVector) = v.len
+function Base.getindex(v::OneHotVector{T}, i::Int) where {T}
+	i == v.n ? one(T) : zero(T)
+end
