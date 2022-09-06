ffdfef2305482ec3d9a05f2cb7adb37ac17c0797
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-09-06 11:55:50 +0400

    triu/l for negative bandwidths (#189)
    * triu/l for negative bandwidths
    
    * Add tests for zero bandwidth

--- test/SpacesTest.jl
+++ test/SpacesTest.jl
@@ -387,6 +387,19 @@ using BandedMatrices: rowrange, colrange, BandedMatrix
             @test (@inferred BandedMatrix(S)) == (@inferred Matrix(S))
         end
 
+
+        @testset "istriu/istril" begin
+            for D in Any[Derivative(Chebyshev()),
+                    Conversion(Chebyshev(), Legendre()),
+                    Multiplication(Fun(Chebyshev()), Chebyshev())]
+                D2 = D[1:3, 1:3]
+                for f in Any[istriu, istril]
+                    @test f(D) == f(D2)
+                    @test f(D') == f(D2')
+                end
+            end
+        end
+
         @testset "inplace ldiv" begin
             @testset for T in [Float32, Float64, ComplexF32, ComplexF64]
                 v = rand(T, 4)
