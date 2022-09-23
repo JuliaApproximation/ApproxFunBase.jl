87bedf5bde0b9d0b0e2e177b6a5da29f01868291
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-09-23 11:16:44 +0530

    require at least one element in tuples

--- src/Operators/spacepromotion.jl
+++ src/Operators/spacepromotion.jl
@@ -77,21 +77,21 @@ promotedomainspace(P::Operator,sp::Space,cursp::Space) =
 
 
 
-function promoterangespace(ops::Union{AbstractVector{O}, NTuple{<:Any,O}}) where O<:Operator
+function promoterangespace(ops::Union{AbstractVector{O}, Tuple{O,Vararg{O}}}) where O<:Operator
     isempty(ops) && return strictconvert(Vector{Operator{eltype(O)}}, ops)
     k=findmaxrangespace(ops)
     #TODO: T might be incorrect
     T=mapreduce(eltype,promote_type,ops)
     Operator{T}[promoterangespace(op,k) for op in ops]
 end
-function promotedomainspace(ops::Union{AbstractVector{O}, NTuple{<:Any,O}}) where O<:Operator
+function promotedomainspace(ops::Union{AbstractVector{O}, Tuple{O,Vararg{O}}}) where O<:Operator
     isempty(ops) && return strictconvert(Vector{Operator{eltype(O)}}, ops)
     k=findmindomainspace(ops)
     #TODO: T might be incorrect
     T=mapreduce(eltype,promote_type,ops)
     Operator{T}[promotedomainspace(op,k) for op in ops]
 end
-function promotedomainspace(ops::Union{AbstractVector{O}, NTuple{<:Any,O}},S::Space) where O<:Operator
+function promotedomainspace(ops::Union{AbstractVector{O}, Tuple{O,Vararg{O}}},S::Space) where O<:Operator
     isempty(ops) && return strictconvert(Vector{Operator{eltype(O)}}, ops)
     k=conversion_type(findmindomainspace(ops),S)
     #TODO: T might be incorrect
