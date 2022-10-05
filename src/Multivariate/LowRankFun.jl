d1a8e82a13a354690dfebfb42077bc6c92ae1a71
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-10-05 11:16:45 +0400

    expand docstring of lowrankfun

--- src/Multivariate/LowRankFun.jl
+++ src/Multivariate/LowRankFun.jl
@@ -7,7 +7,13 @@ export LowRankFun
 """
     LowRankFun(f, space::TensorSpace)
 
-Return an approximation to a bivariate function in low rank form.
+Return an approximation to a bivariate function in a low-rank form
+```math
+f(x,y) = \\sum_i \\sigma_i \\phi_i(x) \\psi_i(y)
+```
+where ``\\sigma_i`` represent the highest singular values,
+and ``\\phi_i(x)`` and ``\\psi_i(y)`` are orthogonal bases. The summation is truncated
+after an acceptable tolerance is reached.
 
 # Examples
 ```jldoctest
