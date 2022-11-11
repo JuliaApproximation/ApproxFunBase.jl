d3454baba7007c6f920030978f3c84f620f9424c
Jishnu Bhattacharya <jishnub.github@gmail.com>
2022-11-11 11:50:26 +0400

    Add conversion test

--- test/SpacesTest.jl
+++ test/SpacesTest.jl
@@ -176,6 +176,7 @@ using LinearAlgebra
             A = 2I : PointSpace(1:4)
             Ainv = inv(A)
             B = convert(Operator{ComplexF64}, Ainv)
+            @test B isa Operator{ComplexF64}
             @test ComplexF64.(Ainv[1:4, 1:4]) == B[1:4, 1:4]
         end
     end
