3275873e140f0afd0ffa77492e4b81017122180b
Jishnu Bhattacharya <jishnub.github@gmail.com>
2022-12-05 15:24:50 +0400

    Add incompatible test

--- test/SpacesTest.jl
+++ test/SpacesTest.jl
@@ -263,6 +263,8 @@ using LinearAlgebra
         @testset "spacescompatible for TensorSpace" begin
             a = PointSpace(1:4)^2
             @test ApproxFunBase.spacescompatible(a, a)
+            b = PointSpace(1:4) ⊗ PointSpace(1:3)
+            @test !ApproxFunBase.spacescompatible(a, b)
         end
     end
 
