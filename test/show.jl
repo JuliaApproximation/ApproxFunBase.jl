bbeadff33a066fd8c9491d4afd006cf9108a05ea
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-03-23 14:16:18 +0530

    update tests

--- test/show.jl
+++ test/show.jl
@@ -68,7 +68,7 @@
 		end
 	end
 	@testset "Iterators" begin
-		B = ApproxFunBase.BlockInterlacer((Ones(2), Ones(2)))
+		B = ApproxFunBase.BlockInterlacer((Ones{Int}(2), Ones{Int}(2)))
 		@test repr(B) == "ApproxFunBase.BlockInterlacer((Ones(2), Ones(2)))"
 		C = cache(B)
 		@test contains(repr(C), "Cached " * repr(B))
