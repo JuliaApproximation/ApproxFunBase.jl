a00c42314de7226fe3b910a9615618e6f07ae4bd
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-04-14 16:11:41 +0530

    use FillArrays.OneElement in basisfunction

--- src/onehotvector.jl
+++ src/onehotvector.jl
@@ -1,3 +1,4 @@
+# TODO: remove once FillArrays compat for <v1 is dropped
 struct OneHotVector{T} <: AbstractVector{T}
 	n :: Int
 	len :: Int
@@ -9,13 +10,18 @@ struct OneHotVector{T} <: AbstractVector{T}
 	end
 end
 OneHotVector(n, len = n) = OneHotVector{Float64}(n, len)
-Base.size(v::OneHotVector) = (v.len,)
-Base.length(v::OneHotVector) = v.len
-function Base.getindex(v::OneHotVector{T}, i::Int) where {T}
+size(v::OneHotVector) = (v.len,)
+length(v::OneHotVector) = v.len
+function getindex(v::OneHotVector{T}, i::Int) where {T}
 	i == v.n ? one(T) : zero(T)
+end
+@static if isdefined(FillArrays, :OneElement)
+	const _OneElement = FillArrays.OneElement
+else
+	const _OneElement = OneHotVector
 end
 # assume that the basis label starts at zero
 function basisfunction(sp, oneindex)
 	oneindex >= 0 || throw(ArgumentError("index to set to one must be non-negative, received $oneindex"))
-	Fun(sp, OneHotVector(oneindex))
+	Fun(sp, _OneElement(oneindex, oneindex))
 end
