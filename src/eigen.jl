2115643ca40e432949f3f3258504b64df30e675f
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-04-26 11:18:43 +0530

    update docstring

--- src/eigen.jl
+++ src/eigen.jl
@@ -9,9 +9,9 @@ eigvals(A::Operator,n::Integer;tolerance::Float64=100eps()) =
 
 Compute the eigenvalues and eigenvectors of the operator `A`. This is done in the following way:
 
-* Truncate `A` into an n×n matrix `A₁`.
+* Truncate `A` into an `n×n` matrix `A₁`.
 * Compute eigenvalues and eigenvectors of `A₁`.
-* Filter out those eigenvectors of `A₁`, which are approximately eigenvectors
+* Filter for those eigenvectors of `A₁` that are approximately eigenvectors
 of `A` as well. The `tolerance` argument controls, which eigenvectors of the approximation are kept.
 """
 function eigs(A::Operator,n::Integer;tolerance::Float64=100eps())
