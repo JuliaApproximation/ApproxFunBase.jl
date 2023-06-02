57ea45ac4ed52a5de04f5d26f3aa608f582540df
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-06-02 17:34:23 +0530

    improve type-inference in mul_coefficients (#466)
    * improve type-inference in mul_coefficients
    
    * special-case Vector branch
    
    * fix function call in _mulcoeff_maybeconvert
    
    * fix eltype promotion
    
    * only convert in out-of-place mul

--- src/Operators/general/algebra.jl
+++ src/Operators/general/algebra.jl
@@ -689,16 +689,11 @@ end
 /(B::Operator, c::Number) = B * inv(c)
 /(B::Operator, c::Fun) = B * inv(c)
 
-
-
-
-
 ## Operations
 for mulcoeff in [:mul_coefficients, :mul_coefficients!]
     @eval begin
         function $mulcoeff(A::Operator, b)
-            n = size(b, 1)
-            ret = n > 0 ? $mulcoeff(view(A, FiniteRange, 1:n), b) : b
+            mulcoeff_maybeconvert($mulcoeff, A, b)
         end
 
         function $mulcoeff(A::TimesOperator, b)
@@ -712,6 +707,18 @@ for mulcoeff in [:mul_coefficients, :mul_coefficients!]
     end
 end
 
+function _mulcoeff_maybeconvert(f::F, A, b) where {F}
+    n = size(b, 1)
+    n > 0 ? f(view(A, FiniteRange, 1:n), b) : b
+end
+
+mulcoeff_maybeconvert(f, A, b) = _mulcoeff_maybeconvert(f, A, b)
+
+function mulcoeff_maybeconvert(f::typeof(mul_coefficients), A, b::Vector)
+    v = _mulcoeff_maybeconvert(f, A, b)
+    T = promote_type(eltype(A), eltype(b))
+    convert(Vector{T}, v)
+end
 
 function *(A::Operator, b)
     ds = domainspace(A)
