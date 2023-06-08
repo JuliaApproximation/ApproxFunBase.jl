94f0c0c192360ed6cf559a3ac5c5a24c65c38788
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-06-08 16:20:56 +0530

    Add docstring for DefiniteIntegral

--- src/Operators/functionals/CalculusFunctional.jl
+++ src/Operators/functionals/CalculusFunctional.jl
@@ -61,6 +61,20 @@ end
 DefiniteIntegral(sp::UnsetSpace) = ConcreteDefiniteIntegral(sp)
 DefiniteLineIntegral(sp::UnsetSpace) = ConcreteDefiniteLineIntegral(sp)
 
+"""
+    DefiniteIntegral([sp::Space])
+
+Return the operator that integrates a `Fun` over its domain. If `sp` is unspecified,
+it is inferred at runtime from the context.
+
+# Examples
+```jldoctest
+julia> f = Fun(x -> 3x^2, Chebyshev());
+
+julia> DefiniteIntegral() * f ≈ 2 # integral of 3x^2 over -1..1
+true
+```
+"""
 function DefiniteIntegral(sp::Space)
     if typeof(canonicaldomain(sp)) == typeof(domain(sp))
         # try using `Integral`
