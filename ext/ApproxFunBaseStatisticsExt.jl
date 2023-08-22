53bee824cfed25ec480165741468e7d4494edfff
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-08-22 11:01:39 +0530

    Revert "Move Statistics to an extension (#566)" (#567)
    This reverts commit d83b4277a7e7c6506dc3a9a49387108306f01152.

--- ext/ApproxFunBaseStatisticsExt.jl
+++
@@ -1,9 +0,0 @@
-module ApproxFunBaseStatisticsExt
-
-import ApproxFunBase
-using ApproxFunBase: IntervalOrSegment
-import Statistics: mean
-
-mean(d::IntervalOrSegment) = ApproxFunBase.mean(d)
-
-end
