6fe969e55cf1c325e3bcb62d8988d44042d46b42
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-06-09 23:57:31 +0530

    Convert ApproxFunBaseTest to a package extension (#657)
    * Convert ApproxFunBaseTest to a package extension
    
    * Add comment
    
    * Ignore Test in downgrade job

---
+++ src/testutils.jl
@@ -0,0 +1,31 @@
+module TestUtils
+
+# methods to the functions defined here are added in ext/ApproxFunBaseTestExt.jl
+
+export testspace, testfunctional, testraggedbelowoperator, testbandedblockbandedoperator,
+    testbandedoperator, testtransforms, testcalculus, testmultiplication, testinfoperator,
+    testblockbandedoperator, testbandedbelowoperator
+
+function testspace end
+
+function testfunctional end
+
+function testraggedbelowoperator end
+
+function testbandedblockbandedoperator end
+
+function testbandedoperator end
+
+function testtransforms end
+
+function testcalculus end
+
+function testmultiplication end
+
+function testinfoperator end
+
+function testblockbandedoperator end
+
+function testbandedbelowoperator end
+
+end
